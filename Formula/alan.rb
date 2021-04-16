052d180faa7ff49544006129ac3ce5e510d584c7
Alejandro Guillen <aguillenv93@gmail.com>
2021-04-16 16:12:40 +0200

    Restore main

--- Formula/alan.rb
+++ Formula/alan.rb
@@ -1,8 +1,8 @@
 class Alan < Formula
   desc "Autoscalable Programming Language"
   homepage "https://github.com/alantech/alan"
-  url "https://github.com/alantech/alan/releases/download/v0.1.37-beta6/alan-macos.tar.gz"
-  sha256 "626ae24239c6602a57237a6326f18de78449f47791210e4c1beed2ddfdbeb775"
+  url "https://github.com/alantech/alan/releases/download/v0.1.36/alan-macos.tar.gz"
+  sha256 "ce1867436faa4434ea75da6377ac9fcf6e4d98539096e92364fd940b7f69a300"
   license "AGPL-3.0"
 
   def install
