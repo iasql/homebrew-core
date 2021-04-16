f219f78f3f2f3ae3d92b401f1d19c16fe0d6de10
Alejandro Guillen <aguillenv93@gmail.com>
2021-04-16 15:31:05 +0200

    Update with relative paths

--- update_formula.sh
+++ update_formula.sh
@@ -5,14 +5,14 @@ wget https://github.com/alantech/alan/releases/download/$1/alan-macos.tar.gz
 ## Get checksum
 checksum=$(openssl sha256 alan-macos.tar.gz | sed 's/.*=//')
 ## Ovewrite formula
-sed -i.bak "s/\/v.*\//\/v$a\//" alan.rb
-sed -i.bak "s/sha256 .*/sha256 $checksum/" alan.rb
+sed -i.bak "s/\/v.*\//\/v$a\//" Formula/alan.rb
+sed -i.bak "s/sha256 .*/sha256 $checksum/" Formula/alan.rb
 # Anycloud
 ## Download released version
 wget https://github.com/alantech/alan/releases/download/$1/anycloud-macos.tar.gz
 ## Get checksum
 checksum=$(openssl sha256 anycloud-macos.tar.gz | sed 's/.*=//')
 ## Ovewrite formula
-sed -i.bak "s/\/v.*\//\/v$a\//" anycloud.rb
-sed -i.bak "s/sha256 .*/sha256 $checksum/" anycloud.rb
+sed -i.bak "s/\/v.*\//\/v$a\//" Formula/anycloud.rb
+sed -i.bak "s/sha256 .*/sha256 $checksum/" Formula/anycloud.rb
 rm *.bak
