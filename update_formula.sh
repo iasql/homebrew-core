825f630f43f627a8e69ca38089124db6adf5da46
Alejandro Guillen <aguillenv93@gmail.com>
2021-04-16 15:36:58 +0200

    Fix update script

--- update_formula.sh
+++ update_formula.sh
@@ -5,14 +5,14 @@ wget https://github.com/alantech/alan/releases/download/$1/alan-macos.tar.gz
 ## Get checksum
 checksum=$(openssl sha256 alan-macos.tar.gz | sed 's/.*=//')
 ## Ovewrite formula
-sed -i.bak "s/\/v.*\//\/v$a\//" Formula/alan.rb
-sed -i.bak "s/sha256 .*/sha256 $checksum/" Formula/alan.rb
+sed -i.bak "s/\/v.*\//\/v$1\//" Formula/alan.rb
+sed -i.bak "s/sha256 .*/sha256 \"$checksum\"/" Formula/alan.rb
 # Anycloud
 ## Download released version
 wget https://github.com/alantech/alan/releases/download/$1/anycloud-macos.tar.gz
 ## Get checksum
 checksum=$(openssl sha256 anycloud-macos.tar.gz | sed 's/.*=//')
 ## Ovewrite formula
-sed -i.bak "s/\/v.*\//\/v$a\//" Formula/anycloud.rb
-sed -i.bak "s/sha256 .*/sha256 $checksum/" Formula/anycloud.rb
+sed -i.bak "s/\/v.*\//\/v$1\//" Formula/anycloud.rb
+sed -i.bak "s/sha256 .*/sha256 \"$checksum\"/" Formula/anycloud.rb
 rm Formula/*.bak
