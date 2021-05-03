6fcffbd17e1d11993e3492ffdbf616e2a315d1b5
depombo <lfdepombo@gmail.com>
2021-05-03 18:58:09 -0400

    bump homebrew version (#6)

--- Formula/alan.rb
+++ Formula/alan.rb
@@ -1,8 +1,8 @@
 class Alan < Formula
   desc "Autoscalable Programming Language"
   homepage "https://github.com/alantech/alan"
-  url "https://github.com/alantech/alan/releases/download/v0.1.36/alan-macos.tar.gz"
-  sha256 "ce1867436faa4434ea75da6377ac9fcf6e4d98539096e92364fd940b7f69a300"
+  url "https://github.com/alantech/alan/releases/download/v0.1.37/alan-macos.tar.gz"
+  sha256 "8feb5d2ac64cebdf529429b6a5d790d38e712417cc7aa1e79d67e42fc2d8470d"
   license "AGPL-3.0"
 
   def install
--- Formula/anycloud.rb
+++ Formula/anycloud.rb
@@ -1,8 +1,8 @@
 class Anycloud < Formula
   desc "Elastically scale webservers in any cloud provider"
   homepage "https://github.com/alantech/anycloud"
-  url "https://github.com/alantech/anycloud/releases/download/v0.1.6/anycloud-macos.tar.gz"
-  sha256 "800f0224a0bb19fe14ded3eb6cf8ee8466266997a79d1490d0729e2710ea404e"
+  url "https://github.com/alantech/alan/releases/download/v0.1.37/anycloud-macos.tar.gz"
+  sha256 "ceb59863a66104c3725437bc52462ced0eed5153b5b72310a47f97750e322cde"
   license "Apache-2.0"
 
   def install
