fd80377d463802e84bc4d0f48542a998732119c2
depombo <lfdepombo@gmail.com>
2021-05-03 19:37:18 -0700

    Update alan.rb

--- Formula/alan.rb
+++ Formula/alan.rb
@@ -2,7 +2,7 @@ class Alan < Formula
   desc "Autoscalable Programming Language"
   homepage "https://github.com/alantech/alan"
   url "https://github.com/alantech/alan/releases/download/v0.1.37/alan-macos.tar.gz"
-  sha256 "8feb5d2ac64cebdf529429b6a5d790d38e712417cc7aa1e79d67e42fc2d8470d"
+  sha256 "8c402b8a595167d89b4a56e806290cff571bf133860cbb2e140f20b07665e63d"
   license "AGPL-3.0"
 
   def install
