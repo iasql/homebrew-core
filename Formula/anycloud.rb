f49a060ebce1475d1ca50e2ce921cff00b14f847
depombo <lfdepombo@gmail.com>
2021-05-03 19:35:38 -0700

    Update anycloud.rb

--- Formula/anycloud.rb
+++ Formula/anycloud.rb
@@ -2,7 +2,7 @@ class Anycloud < Formula
   desc "Elastically scale webservers in any cloud provider"
   homepage "https://github.com/alantech/anycloud"
   url "https://github.com/alantech/alan/releases/download/v0.1.37/anycloud-macos.tar.gz"
-  sha256 "ceb59863a66104c3725437bc52462ced0eed5153b5b72310a47f97750e322cde"
+  sha256 "29cdb976d630ef170eeafda96d7c04a0c8715c337abce473d13adefe633e8ada"
   license "Apache-2.0"
 
   def install
