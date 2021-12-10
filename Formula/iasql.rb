589f82e12a07da16b7e7d27824975bf73239e121
depombo <depombo@users.noreply.github.com>
2021-12-10 17:35:17 +0000

    Bump version v0.2.0

--- Formula/iasql.rb
+++ Formula/iasql.rb
@@ -1,8 +1,8 @@
 class Iasql < Formula
   desc "Infrastructure as SQL"
   homepage "https://iasql.com"
-  url "https://github.com/iasql/releases/releases/download/v0.1.0/iasql-macos.tar.gz"
-  sha256 "611e9c387f7c635ba404c808eadcbd289e20fbf71be419b04ad2da04e1389094"
+  url "https://github.com/iasql/releases/releases/download/v0.2.0/iasql-macos.tar.gz"
+  sha256 "14ac77bcab22523b5f4fe8f24c20221bf011fbad41a8595fcbc6d96cc9c5708a"
 
   def install
     puts "Installing IaSQL..."
